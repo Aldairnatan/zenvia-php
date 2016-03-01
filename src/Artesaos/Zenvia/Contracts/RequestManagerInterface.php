ad08573c57275d710b818c30305ebef1eddf383e
Mauri de Souza Nunes <mauri870@gmail.com>
2016-02-29 22:18:14 -0300

    Add interface for RequestManager

---
+++ src/Artesaos/Zenvia/Contracts/RequestManagerInterface.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mauri870
+ * Date: 29/02/16
+ * Time: 22:17
+ */
+
+namespace Artesaos\Zenvia\Contracts;
+
+
+interface RequestManagerInterface
+{
+
+}
