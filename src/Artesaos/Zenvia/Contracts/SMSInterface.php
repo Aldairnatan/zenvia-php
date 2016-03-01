c7cb8b39da591df204d972d444d637cc50af919e
Mauri de Souza Nunes <mauri870@gmail.com>
2016-03-01 20:47:36 -0300

    Add simple_xml response type to docblock

--- src/Artesaos/Zenvia/Contracts/SMSInterface.php
+++ src/Artesaos/Zenvia/Contracts/SMSInterface.php
@@ -9,7 +9,7 @@ interface SMSInterface
      * Send a SMS
      *
      * @param array $body
-     * @param string $responseFormat The response format must be one of: array, json, string, stream, psr7
+     * @param string $responseFormat The response format must be one of: array, json, string, stream, psr7, simple_xml
      * @return \Psr\Http\Message\ResponseInterface
      */
     public function send(array $body, $responseFormat = 'json');
