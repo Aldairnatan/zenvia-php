3bb7e5f722f32c19c4cb0bfa21fa37f8a7cfcad6
Mauri de Souza Nunes <mauri870@gmail.com>
2016-03-01 11:17:12 -0300

    Add method for set http client and send psr-7 requests

--- src/Artesaos/Zenvia/Contracts/RequestManagerInterface.php
+++ src/Artesaos/Zenvia/Contracts/RequestManagerInterface.php
@@ -3,7 +3,28 @@
 namespace Artesaos\Zenvia\Contracts;
 
 
+use Artesaos\Zenvia\RequestManager;
+use Http\Client\HttpClient;
+
 interface RequestManagerInterface
 {
+    /**
+     * Send a request.
+     *
+     * @param string $method
+     * @param string $uri
+     * @param array  $headers
+     * @param string $body
+     * @param string $protocolVersion
+     *
+     * @return \Psr\Http\Message\ResponseInterface
+     *
+     */
+    public function sendRequest($method, $uri, array $headers = [], $body = null, $protocolVersion = '1.1');
 
+    /**
+     * @param \Http\Client\HttpClient $httpClient
+     * @return RequestManager
+     */
+    public function setHttpClient(HttpClient $httpClient);
 }
